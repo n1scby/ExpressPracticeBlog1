f944be7e531249d188ac89cb4fe1140cb0c7e039
Nic B <n1scby@yahoo.com>
2017-12-15 14:51:02 -0500

    Now can see the blog entries and add entries.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var about = require('./routes/about');
+var post = require('./routes/post');
+var addPost = require('./routes/addPost');
 
 var app = express();
 
@@ -26,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/about', about);
+app.use('/post', post);
+app.use('/addPost', addPost);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/blogRepo.js
+++ models/blogRepo.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require('path');
-filePath = path.join(__dirname, "data");
-fileName = path.join(filePath, "postData.json");
+const filePath = path.join(__dirname, "data");
+const fileName = path.join(filePath, "postData.json");
 
-let blogList = [
+/* let blogList = [
     {linkName: "blog1",
     title: "Blog1 Title",
     postContent: "<h1>Blog 1</h1><p>Welcome to Blog 1.  This is an awesome blog.  Check out Blog #2 for a more awesome post.</p>",
@@ -12,7 +12,35 @@ let blogList = [
     title: "Blog2 Title",
     postContent: "<h2>Blog 2</h2><p>Welcome to Blog 2.  Wow! You are so lucky to be here.  This is so super awesome! </p>", 
     author: "Me"}
-];
+]; */
+
+let blogList = [];
+
+let savePosts = ()=>{
+    fs.writeFile(fileName, JSON.stringify(blogList), (err)=>{
+        if(err) {
+            console.log("Error writing file!  Abort!" + err.message);
+            throw(err);
+        }else{
+            console.log("The file: " + fileName + " has been saved.  Sweet!");
+        }
+    });
+};
+
+let loadPosts = () =>{
+    fs.readFile(fileName,"utf8", (err, data)=>{
+        if(err) {
+            console.log("Error reading file.  Darnit! " + err.message);
+            throw err;
+        }else {
+            console.log("File has been loaded!  Yay!");
+            let loadPosts = JSON.parse(data);
+            blogList = loadPosts;
+        }
+
+    });
+};
+
 
 let repository = {
     dataSource : "Filesystem",
@@ -25,9 +53,13 @@ let repository = {
     },
     addPost: (newBlog) => {
         blogList.push(newBlog);
+        savePosts();
+
     
     }
 }
+
+loadPosts();
 
 module.exports = repository;
 
